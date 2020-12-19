a7a0dfd550ab0bd5ccac48f3de6c0cd3f4de9641
Makanak4 <losos43rus@gmail.com>
2020-12-19 20:58:39 +0300

    update test

--- src/test/java/tech/itpark/timetracking/TimetrackingApplicationTests.java
+++ src/test/java/tech/itpark/timetracking/TimetrackingApplicationTests.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -22,38 +20,38 @@ class TimetrackingApplicationTests {
     void crudTest() throws Exception {
         mockMvc.perform(get("/employee"))
                 .andExpect(content()
-                    .json("[\n" +
-                "  {\n" +
-                "    \"id\": 1,\n" +
-                "    \"name\": \"Ivan Ivanov\",\n" +
-                "    \"position\": \"Manager\",\n" +
-                "    \"clock_rate\": 162\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": 2,\n" +
-                "    \"name\": \"Petr Petrov\",\n" +
-                "    \"position\": \"Manager\",\n" +
-                "    \"clock_rate\": 162\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": 3,\n" +
-                "    \"name\": \"Maksim Maksimov\",\n" +
-                "    \"position\": \"Administrator\",\n" +
-                "    \"clock_rate\": 162\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": 4,\n" +
-                "    \"name\": \"Elena Nikolaevna\",\n" +
-                "    \"position\": \"Bookkeeper\",\n" +
-                "    \"clock_rate\": 162\n" +
-                "  },\n" +
-                "  {\n" +
-                "    \"id\": 5,\n" +
-                "    \"name\": \"Olga Tarasova\",\n" +
-                "    \"position\": \"Human resources\",\n" +
-                "    \"clock_rate\": 162\n" +
-                "  }\n" +
-                "]"));
+                        .json("[\n" +
+                                "  {\n" +
+                                "    \"id\": 1,\n" +
+                                "    \"name\": \"Ivan Ivanov\",\n" +
+                                "    \"position\": \"Manager\",\n" +
+                                "    \"clock_rate\": 162\n" +
+                                "  },\n" +
+                                "  {\n" +
+                                "    \"id\": 2,\n" +
+                                "    \"name\": \"Petr Petrov\",\n" +
+                                "    \"position\": \"Manager\",\n" +
+                                "    \"clock_rate\": 162\n" +
+                                "  },\n" +
+                                "  {\n" +
+                                "    \"id\": 3,\n" +
+                                "    \"name\": \"Maksim Maksimov\",\n" +
+                                "    \"position\": \"Administrator\",\n" +
+                                "    \"clock_rate\": 162\n" +
+                                "  },\n" +
+                                "  {\n" +
+                                "    \"id\": 4,\n" +
+                                "    \"name\": \"Elena Nikolaevna\",\n" +
+                                "    \"position\": \"Bookkeeper\",\n" +
+                                "    \"clock_rate\": 162\n" +
+                                "  },\n" +
+                                "  {\n" +
+                                "    \"id\": 5,\n" +
+                                "    \"name\": \"Olga Tarasova\",\n" +
+                                "    \"position\": \"Human resources\",\n" +
+                                "    \"clock_rate\": 162\n" +
+                                "  }\n" +
+                                "]"));
 
         mockMvc.perform(get("/employee/1"))
                 .andExpect(content()
@@ -65,7 +63,7 @@ class TimetrackingApplicationTests {
                                 "}"));
 
         mockMvc.perform(get("/employee/search")
-            .queryParam("name", "Maksim Maksimov")
+                .queryParam("name", "Maksim Maksimov")
         )
                 .andExpect(content()
                         .json(
@@ -79,7 +77,7 @@ class TimetrackingApplicationTests {
                                         "]"));
 
         mockMvc.perform(post("/employee")
-            .contentType("application/json")
+                .contentType("application/json")
                 .content("{\n" +
                         "  \"id\":0,\n" +
                         "  \"name\":\"Pet\",\n" +
@@ -95,5 +93,33 @@ class TimetrackingApplicationTests {
                                         "  \"position\": \"Writer\",\n" +
                                         "  \"clock_rate\": 160\n" +
                                         "}"));
+
+        mockMvc.perform(post("/employee")
+                .contentType("application/json")
+                .content("{\n" +
+                        "  \"id\":6,\n" +
+                        "  \"name\":\"Rick\",\n" +
+                        "  \"position\":\"Writer\",\n" +
+                        "  \"clock_rate\":160\n" +
+                        "}")
+        )
+                .andExpect(content()
+                        .json(
+                                "{\n" +
+                                        "  \"id\": 6,\n" +
+                                        "  \"name\": \"Rick\",\n" +
+                                        "  \"position\": \"Writer\",\n" +
+                                        "  \"clock_rate\": 160\n" +
+                                        "}"));
+
+        mockMvc.perform(delete("/employee/6"))
+                .andExpect(content()
+                        .json("{\n" +
+                                "  \"id\":6,\n" +
+                                "  \"name\":\"Rick\",\n" +
+                                "  \"position\":\"Writer\",\n" +
+                                "  \"clock_rate\":160" +
+                                "\n" +
+                                "}"));
     }
 }
